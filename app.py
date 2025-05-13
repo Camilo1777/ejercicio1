0a25ea3ee3a17ddcbdbe1405eaadce9b29785142
Camilo1777 <141794560+Camilo1777@users.noreply.github.com>
2025-05-13 23:59:53 +0000

    Primera parte del proyecto

---
+++ app.py
@@ -0,0 +1,42 @@
+
+
+
+import requests
+import configparser
+
+
+#Habilita capacidades 
+
+
+
+
+
+
+app = Flask(__name__)
+
+#Iniciamos con la logica de la aplicacion 
+
+@app.route ('/')
+
+#Aqui va el nombre de la funcion o metodo que gestiona la ruta 
+def weather_dashboard():
+    return render_template ('home.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=true)
